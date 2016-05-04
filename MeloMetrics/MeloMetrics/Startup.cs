a6e26b178cd06771e72cfe2daad900488d2db42d
Yeison Melo <melogarmin@hotmail.com>
2016-05-05 01:53:50 +0200

    primer commit

---
+++ MeloMetrics/MeloMetrics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MeloMetrics.Startup))]
+namespace MeloMetrics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
